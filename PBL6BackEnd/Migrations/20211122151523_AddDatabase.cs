f6e5e5ce0a1fcd264ef93f13ae26e117adc30ed9
luuhieu8720 <lethiluuhieu@gmail.com>
2021-11-26 00:23:43 +0700

    update ssl

--- PBL6BackEnd/Migrations/20211122151523_AddDatabase.cs
+++ PBL6BackEnd/Migrations/20211122151523_AddDatabase.cs
@@ -24,11 +24,6 @@ namespace PBL6BackEnd.Migrations
                 {
                     table.PrimaryKey("PK_User", x => x.Id);
                 });
-
-            migrationBuilder.InsertData(
-                table: "Users",
-                columns: new[] { "Id", "BirthDay", "FirstName", "LastName", "Password", "Phone", "Role", "Username" },
-                values: new object[] { new Guid("00000000-0000-0000-0000-000000000001"), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Admin", "01", "d34b21af1ebb547742f2a78124c73764", null, 2, "admin" });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
