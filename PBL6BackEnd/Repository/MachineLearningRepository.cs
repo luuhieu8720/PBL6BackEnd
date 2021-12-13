a567576797c6ad80741dec2e8360c8607108c3f7
luuhieu8720 <lethiluuhieu@gmail.com>
2021-12-13 14:56:40 +0700

    Update API

--- PBL6BackEnd/Repository/MachineLearningRepository.cs
+++ PBL6BackEnd/Repository/MachineLearningRepository.cs
@@ -24,7 +24,7 @@ namespace PBL6BackEnd.Repository
         public async Task<ResultResponse> Get(RequestForm requestForm)
         {
             using var client = new HttpClient();
-            client.BaseAddress = new Uri("http://5934-34-79-185-15.ngrok.io/");
+            client.BaseAddress = new Uri("http://0d43-35-222-20-145.ngrok.io/");
             
             var responseTask = client.PostAsJsonAsync("predict/image", requestForm);
             responseTask.Wait();
